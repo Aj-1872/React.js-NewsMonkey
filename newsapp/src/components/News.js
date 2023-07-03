1417bdadb53b771012365aaee0b87132f4260ca5
Aj-1872 <ajayjodedra@gmail.com>
2023-07-03 15:54:37 -0700

    adding components

---
+++ newsapp/src/components/News.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export default class News extends Component {
+  static propTypes = {}
+
+  render() {
+    return (
+      <div>
+
+             this is components
+             <NewsItem/>
+
+      </div>
+    )
+  }
+}
