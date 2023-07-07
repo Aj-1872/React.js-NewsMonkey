95b555f7708bcf5502b808a3299c30a207ffbbc5
Aj-1872 <ajayjodedra@gmail.com>
2023-07-06 18:55:32 -0700

    fetching new by using API

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -4,20 +4,24 @@ export class NewsItem extends Component {
 
   render() {
 
-  let  {title,description,imgUrl,newsUrl} = this.props;
+    let { title, description, imgUrl, newsUrl } = this.props;
 
     return (
-      <div>
-        <div className="card" style={{width: "18rem"}}>
-          <img src={imgUrl} className="card-img-top" alt="..."/>
-            <div className="card-body">
-              <h5 className="card-title">{title}</h5>
-              <p className="card-text">{description}</p>
-              <a href={newsUrl} target='_blank' className="btn btn-sm btn-primary">Read More</a>
-            </div>
-        </div>
 
+
+      <div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={!imgUrl ? "https://via.placeholder.com/300" : imgUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href={newsUrl} target='_blank' className="btn btn-sm btn-primary">Read More</a>
+          </div>
+        </div>
       </div>
+
+
+
     )
   }
 }
